3f3387648247e8f04b6f90c0bb9bf53c803acff8
Ali Hamza <alihamza00053@gmail.com>
2023-11-14 17:24:39 +0500

    onBackPress
    close app and turn off flash if running on back press

--- FlashLightApp/app/src/main/java/com/flashlightapp/a/h/MainActivity.kt
+++ FlashLightApp/app/src/main/java/com/flashlightapp/a/h/MainActivity.kt
@@ -1,41 +1,26 @@
 package com.flashlightapp.a.h
 
 import android.content.Context
-import android.hardware.camera2.CameraAccessException
-import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.flashlightapp.a.h.ui.theme.FlashLightAppTheme
@@ -43,29 +28,38 @@ import com.flashlightapp.a.h.ui.theme.FlashLightAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var cameraManager =
+            applicationContext.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        var cameraID = cameraManager.cameraIdList[0]
+
         setContent {
             FlashLightAppTheme {
                 // A surface container using the 'background' color from the theme
-                FlashLight()
+                    FlashLight(cameraManager, cameraID)
             }
         }
+    }
+
+    override fun onBackPressed() {
+        var cameraManager =
+            applicationContext.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        var cameraID = cameraManager.cameraIdList[0]
+        cameraManager.setTorchMode(cameraID, false)
+        finishAffinity()
+        super.onBackPressed()
     }
 }
 
 
 @Composable
-fun FlashLight() {
-    var context = LocalContext.current
+fun FlashLight(cameraManager: CameraManager, cameraID: String) {
     var isFlashOn = remember {
         mutableStateOf(false)
     }
     var flashOnOff = remember {
         mutableStateOf("Off")
     }
-    var camera: Camera? = null
-
-    var cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
-    var cameraID = cameraManager.cameraIdList[0]
 
     Column(
         modifier = Modifier.fillMaxSize(),
